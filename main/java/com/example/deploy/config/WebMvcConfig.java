9593b4d096339c1da949636c2b6f2d41b5f8225f
kwonkb <kwonkb98@gmail.com>
2024-05-14 11:38:03 +0900

    commit

---
+++ main/java/com/example/deploy/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.example.deploy.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
